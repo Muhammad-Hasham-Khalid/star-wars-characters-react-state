42d91a88fbe491e0b6aa32252470f6c857cfa712
Muhammad-Hasham-Khalid <m.hasham681@gmail.com>
2020-09-07 02:29:35 +0500

    useEffect with promise and async

--- src/index.js
+++ src/index.js
@@ -1,16 +1,57 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-
+import endpoint from './endpoint';
 import { BrowserRouter as Router } from 'react-router-dom';
-
 import CharacterList from './CharacterList';
-
 import dummyData from './dummy-data';
-
 import './styles.scss';
 
+const useFetch = (url) => {
+  const [response, setResponse] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  // we can't make this function in useEffect async
+  React.useEffect(() => {
+    // settings all the states
+    setLoading(true);
+    setResponse(null);
+    setError(null);
+
+    const fetchUrl = async () => {
+      try {
+        const reponse = await fetch(url);
+        const data = await reponse.json();
+        setResponse(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    // calling the function
+    fetchUrl();
+
+    // fetch(endpoint + '/characters')
+    //   .then((response) => response.json())
+    //   .then((response) => {
+    //     setLoading(false);
+    //     setResponse(response);
+    //   })
+    //   .catch((error) => {
+    //     setLoading(false);
+    //     setError(error);
+    //   });
+  }, []);
+
+  return [response, loading, error];
+};
+
 const Application = () => {
-  const [characters, setCharacters] = useState(dummyData);
+  const [response, loading, error] = useFetch(endpoint + '/characters');
+
+  const characters = (response && response.characters) || [];
 
   return (
     <div className="Application">
@@ -19,7 +60,12 @@ const Application = () => {
       </header>
       <main>
         <section className="sidebar">
-          <CharacterList characters={characters} />
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            <CharacterList characters={characters} />
+          )}
+          {error && <p className="error">{error.message} </p>}
         </section>
       </main>
     </div>
